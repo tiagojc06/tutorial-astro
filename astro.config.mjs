c7a92ad33fc8182494ad235d385d0e9082d101cb
Tiago Castelo <tiagojcastelo@gmail.com>
2026-09-25 22:11:02 -0300

    - Se creo una plantilla base que contiene el head, Header, título principal (h1) con su texto pasado como prop, Footer, importación del script para hacer funcionar al botón Menu y del CSS con los estilos globales. Se incluyo un slot de nombre `head` para que páginas puedan agregar dinámicamente otras metaetiquetas y demás. - Se importo y uso la plantilla base en todas las páginas incluidos los post MD. En la página `about` se mantuvieron los estilos específicos, para que se apliquen correctamente se agrego la directiva is:global ya que por defecto las reglas CSS en Astro aplican solo al HTML escrito en el mismo componente mediante la asignación de un atributo data-astro-cid-* igual para todos los elementos HTML dentro de ese mismo componente que tengan asociada una regla CSS. Como el h1 es usado en BaseLayout es necesaria la directiva para que no se agregue el atributo data-astro-cid-* y hacer que las reglas CSS aplicadas para tales elementos dentro de esa página funcionen. Un problema similar pasaría para los elementos de clase `skill` si hubiera puesto la etiqueta style dentro del slot `head` de la plantilla sin la directiva is:global, por eso para simplificar decidí dejar la etiqueta style en el slot default para tener una única razón para usar el is:global. - Se creo una plantilla para los post MD que usa los datos de sus frontmatters y que a su vez usa la plantilla base. - En la configuración de Astro se definió el dominio `docs.astro.build` para que el componente Image de Astro pueda optimizar imagenes remotas. Originalmente, como es obligatorio definir un tamaño para imagenes remotas, lo hice para poder usar el atributo `inferSize` aunque después me decidí por definirles un width y height fijo manualmente.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    // Define una lista de dominios de origen de imágenes permitidos para la optimización remota de imágenes. Astro no optimizará ninguna otra imagen remota.
+    domains: ["docs.astro.build"],
+  },
+});
